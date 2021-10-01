aa05cc9ca2778b9107bf6740ed3e5779a0b5c8fc
marina-zar <panzar.marina@gmail.com>
2021-10-01 09:11:25 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+// import * as $ from 'jquery';
+/***** Import custom style ****/
+import './assets/css/fonts.css'
+import './assets/scss/style.scss'
+
+
+/***** Import JavaScript ****/
+import bootstrap from 'bootstrap/dist/js/bootstrap.bundle';

71f54011ac6530bde08f961fdfb80dda7fe2b3c8
marina-zar <panzar.marina@gmail.com>
2021-11-12 13:34:24 +0200

    made dropdown

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
-// import * as $ from 'jquery';
+
 /***** Import custom style ****/
 import './assets/css/fonts.css'
 import './assets/scss/style.scss'
 
 
-/***** Import JavaScript ****/
-import bootstrap from 'bootstrap/dist/js/bootstrap.bundle';
+
--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,7 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 const CssMinimizerWebpackPlugin = require('css-minimizer-webpack-plugin');
 const TerserWebpackPlugin = require('terser-webpack-plugin');
 const HtmlWebpackPartialsPlugin = require('html-webpack-partials-plugin');
+const webpack = require('webpack');
 
 const isDev = process.env.NODE_ENV === 'development';
 
@@ -29,7 +30,7 @@ const optimization = () => {
     return config
 };
 
-const filename = ext => isDev ? `[name].${ext}` : `[name].[fullhash].${ext}`;
+const filename = ext => isDev ? `[name].${ext}` : `[name].[chunkhash].${ext}`;
 
 const cssLoaders = extra => {
     let loaders = [
@@ -46,19 +47,23 @@ const cssLoaders = extra => {
 
 let conf = {
     mode: "development",
+    // watch: isDev,
     entry: {
+        vendor: './src/vendor.js',
         main: './src/index.js',
-        // product: './src/product/index.js'
     },
     output: {
         path: path.resolve(__dirname, './dist'),
         filename: filename('js'),
-        publicPath: ""
+        publicPath: "",
+        clean: true,
     },
     optimization: optimization(),
     devServer: {
         port: 8081,
-        contentBase: path.resolve(__dirname, 'dist'),
+        static: {
+            directory: path.resolve(__dirname, 'dist'),
+        },
         // hot: isDev,
         // overlay: {
         //     warnings: true,
@@ -81,8 +86,17 @@ let conf = {
                 use: cssLoaders(),
             },
             {
-                test: /\.(png|jpg|svg|gif)$/,
-                use: ['file-loader']
+                test: /\.(png|jpg|svg|gif)$/i,
+                use: [
+                    'file-loader',
+                    {
+                        loader: 'image-webpack-loader',
+                        options: {
+                            // bypassOnDebug: true, // webpack@1.x
+                            // disable: true, // webpack@2.x and newer
+                        },
+                    },
+                ]
             },
             {
                 test: /\.(ttf|otf|woff|woff2|eot)$/,
@@ -109,7 +123,6 @@ let conf = {
                 collapseWhitespace: isProd
             },
             cache: false,
-            // chunks: ['index']
         }),
         new HtmlWebpackPartialsPlugin({
             path: path.join(__dirname, './src/partials/header.html'),
@@ -121,7 +134,7 @@ let conf = {
         //     location: 'aside',
         //     template_filename: ['index.html', 'product.html']
         // }),
-        new CleanWebpackPlugin(),
+        // new CleanWebpackPlugin(),
         new CopyWebpackPlugin({
             patterns: [
                 {
@@ -133,6 +146,9 @@ let conf = {
                     to: path.resolve(__dirname, 'dist'),
                 }
             ],
+        }),
+        new webpack.DefinePlugin({
+            'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
         }),
     ],
 };
